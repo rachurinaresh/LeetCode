35da74e3b5458ea67c20afcd2f20b31061934757
rachurinaresh <rachurinaresh249@gmail.com>
2025-11-20 23:22:21 -0600

    1048 code change

--- 1048. Longest String Chain.py
+++ 1048. Longest String Chain.py
@@ -1,21 +1,20 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        words.sort(key=lambda w:-len(w))
         word_index = {}
+        dp = {}
         for i,w in enumerate(words):
             word_index[w]=i
-        dp = {}
         def dfs(i):
             if i in dp:
                 return dp[i]
+            w = words[i]
             res = 1
-            for j in range(len(words[i])):
-                w = words[i]
-                pred = w[:j]+w[j+1:]
+            for j in range(len(w)):
+                pred = w[:j] + w[j+1:]
                 if pred in word_index:
-                    res = max(res,1 + dfs(word_index[pred]))
+                    res = max(res, 1 + dfs(word_index[pred]))
             dp[i] = res
             return res
-        for i in range(len(words)):
+        for i in range(len(words)-1, -1 ,-1):
             dfs(i)
         return max(dp.values())
